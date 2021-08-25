deaf7bf8068cd6c3782def8a4cdbf8468f1b83fc
shane <willyums86@gmail.com>
2021-08-25 13:31:42 -0400

    Adds single product route

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,4 +3,8 @@ class ProductsController < ApplicationController
     product = Product.all
     render json: product.as_json
   end
+  def one_product
+    product = Product.find(2)
+    render json: product.as_json
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   get "/products", controller: "products", action: "index"
+  get "/one_product", controller: "products", action: "one_product"
 end
